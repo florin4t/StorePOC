e849e5caa8d20575aba15a4646138c1632f4e69d
Florin Sirbu <florin.sirbu@tremend.com>
2022-11-08 16:42:56 +0200

    Create a maven project with Spring Initializr and a few dependecies

---
+++ src/test/java/com/management/store/ManagementApp/ManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.store.ManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
