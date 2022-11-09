71cb768b2a94642e70e38a773a19b946fdc8c941
Florin Sirbu <florin.sirbu@tremend.com>
2022-11-09 17:18:39 +0200

    adding a test for getting products when some products exist

--- src/test/java/com/management/store/rest/controller/StoreProductControllerTest.java
+++ src/test/java/com/management/store/rest/controller/StoreProductControllerTest.java
@@ -1,20 +1,25 @@
 package com.management.store.rest.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.management.store.app.ManagementAppApplication;
+import com.management.store.model.entity.StoreProduct;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = StoreProductController.class)
 @ContextConfiguration(classes = ManagementAppApplication.class)
@@ -23,12 +28,38 @@ public class StoreProductControllerTest {
     @Autowired
     private MockMvc mock;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @SneakyThrows
     @Test
-    void products() {
+    void productsEmptyList() {
         mock.perform(get("/api/product/all")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(List.of().toString()));
+    }
+
+    @SneakyThrows
+    @Test
+    @DirtiesContext
+    void productsNonEmptyList() {
+        int itemsToCreate = (int) (Math.random() * 10);
+        Stream.iterate(0, i -> i + 1)
+                .limit(itemsToCreate)
+                .forEach(i -> callAddEndpoint());
+
+        mock.perform(get("/api/product/all")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(itemsToCreate)));
+    }
+
+    @SneakyThrows
+    private void callAddEndpoint() {
+        mock.perform(post("/api/product/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new StoreProduct())))
+                .andExpect(status().isOk());
     }
 }
