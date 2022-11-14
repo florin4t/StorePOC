cfe9077448a5e6f7bfade6887e8987d32bd7d3b4
Florin Sirbu <florin.sirbu@tremend.com>
2022-11-14 09:50:01 +0200

    something I could have done differently - starting the API design with an interface adding an unsupporeted delete all operation

--- src/main/java/com/management/store/rest/controller/StoreProductController.java
+++ src/main/java/com/management/store/rest/controller/StoreProductController.java
@@ -1,6 +1,7 @@
 package com.management.store.rest.controller;
 
 import com.management.store.model.entity.StoreProduct;
+import com.management.store.rest.api.StoreProductManagementApi;
 import com.management.store.rest.service.StoreProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
-public class StoreProductController {
+public class StoreProductController implements StoreProductManagementApi {
 
     private final StoreProductService storeProductService;
 
@@ -22,24 +23,28 @@ public class StoreProductController {
         this.storeProductService = storeProductService;
     }
 
+    @Override
     @PreAuthorize("hasRole('BASIC')")
     @GetMapping("/all")
     public List<StoreProduct> products() {
         return this.storeProductService.getStoreProducts();
     }
 
+    @Override
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
     public StoreProduct addProduct(@RequestBody @Valid StoreProduct newProduct) {
         return this.storeProductService.addProduct(newProduct);
     }
 
+    @Override
     @PreAuthorize("hasRole('BASIC')")
     @GetMapping("/{id}")
     public ResponseEntity<List<StoreProduct>> find(@PathVariable(name = "id") String productId) {
         return new ResponseEntity<>(this.storeProductService.getProduct(productId), HttpStatus.OK);
     }
 
+    @Override
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public List<StoreProduct> updateProduct(@PathVariable(name = "id") String productId,
@@ -47,10 +52,16 @@ public class StoreProductController {
         return this.storeProductService.updateProduct(productId, updatedProduct);
     }
 
+    @Override
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable(name = "id") String productId) {
         this.storeProductService.deleteProduct(productId);
         return new ResponseEntity<>(String.format("Deleted product with ID %s", productId), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<String> deleteAllProducts() {
+        return StoreProductManagementApi.super.deleteAllProducts();
     }
 }
